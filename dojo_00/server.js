45a18b129accce3bed8391c809d6187c82508277
brunoferraz <brunoferraz.pro@gmail.com>
2020-04-11 13:59:00 -0300

    [1] nodemon and nunjucks installed

---
+++ dojo_00/server.js
@@ -0,0 +1,20 @@
+//configurando o servidor
+const express = require("express")
+const server = express()
+
+//congurando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+})
+
+
+//configurando a apresentacao da pagina
+server.get("/", function(req, res){
+    return res.render("index.html")
+})
+
+//liga os ervidor e permite o acesso na porta 3000
+server.listen(3000, function(){
+    console.log("SERVIDOR Funcionando")
+})
